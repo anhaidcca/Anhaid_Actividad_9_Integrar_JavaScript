ae03075cd8e0f07c29ee6165825c0d6ee95df13e
anhaidcca <anhaidcca@gmail.com>
2020-12-14 18:26:14 -0600

    14dic

---
+++ funciones.js
@@ -0,0 +1,12 @@
+console.log("Primera práctica");
+
+var Prueba1 = "Variable con var";
+let Prueba2 = "Variable con let";
+const Prueba3 = "Variable con const";
+
+console.log(Prueba1);
+console.log(Prueba2);
+console.log(Prueba3);
+
+var Prueba1 = "Variable con var";
+Prueba3 = "Nuevo valor de variable con const";
